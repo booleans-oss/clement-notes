7cc75fe5d4146679f6bbe76568030f3eaf22d35c
booleans-oss <ztafade@gmail.com>
2023-05-26 21:54:26 +0200

    remove monitoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,7 @@ import "@/styles/globals.css";
 import 'katex/dist/katex.min.css'
 // import { ClerkProvider } from "@clerk/nextjs";
 
-import logRocket from 'logrocket';
-import setupLogRocketReact from 'logrocket-react';
-
 const MyApp: AppType = ({ Component, pageProps }) => {
-  if (typeof window !== 'undefined') {
-    logRocket.init('xgugta/clement-notes');
-    setupLogRocketReact(logRocket);
-  }
   return (
     <>
     {/* <ClerkProvider> */ }
