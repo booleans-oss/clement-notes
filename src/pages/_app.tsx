5f53339dcc128a6a1ad98b0fd16b2e412608f436
booleans-oss <ztafade@gmail.com>
2023-09-17 15:26:55 +0200

    add redirection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,31 @@
 import { type AppType } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
+import { useEffect } from "react";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 import "katex/dist/katex.min.css";
-import { ClerkProvider } from "@clerk/nextjs";
+
+const url = "https://www.clement.best";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const a = document.createElement("a");
+      if (!a.click) {
+        //for IE
+        window.location.href = url;
+        return;
+      }
+      a.setAttribute("href", url);
+      a.style.display = "none";
+      document.body.appendChild(a);
+      a.click();
+    }
+  }, []);
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 };
